45e391dbaf08ec257d91a70973cb087334811dce
Miguel Solorio <miguel.solorio07@gmail.com>
2015-04-10 09:57:42 -0700

    Added 'gulp-wait'
    Added a delay to allow export files to finish and allow for
    Illustrator's "Save For Web" export process to complete. I also targeted
    PNG files so that temp files do not get processed.

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
 var gulp = require('gulp'),
     newer = require('gulp-newer'),
+    wait = require('gulp-wait'),
     imagemin = require('gulp-imagemin');
 
-var imgSrc = 'assets/*';
-var imgDest = '_optimized';
+var imgSrc = 'assets/*.png';
+var imgDest = 'assets-opt/';
 
 // Imagemin
 gulp.task('images', function() {
   return gulp.src(imgSrc)
     .pipe(newer(imgDest))
+    .pipe(wait(2000))
     .pipe(imagemin({
         optimizationLevel: 7,
         progressive: true,
